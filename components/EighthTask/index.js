d2ec4c53f6fd66483f23faf503ec6c8d27a1c7a8
GreedoRen <greedo.it@gmail.com>
2019-08-29 17:15:37 +0300

    Eighth task

--- components/EighthTask/index.js
+++ components/EighthTask/index.js
@@ -1,4 +1,8 @@
 document.getElementById('toggle').onclick = function() {
-	let div = document.getElementById('div1');
-	div.style.display = div.style.display === 'none' ? 'block' : 'none';
+	let div1 = document.getElementById('div1');
+	let div2 = document.getElementById('div2');
+	let div3 = document.getElementById('div3');
+	div1.style.display = div1.style.display === 'none' ? 'block' : 'none';
+	div2.style.display = div2.style.display === 'none' ? 'block' : 'none';
+	div3.style.display = div3.style.display === 'none' ? 'block' : 'none';
 };
