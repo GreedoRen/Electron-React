e25757f5a43678ca35c819f5a37cf3e59812dde8
GreedoRen <greedo.it@gmail.com>
2019-09-04 21:21:58 +0300

    Start electron

--- src/App.js
+++ src/App.js
@@ -1,38 +1,13 @@
 import React, { Component } from 'react';
 
-const { ipcRenderer } = window.require('electron');
-
 class App extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-        arg: ''
-      };
-      this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-      ipcRenderer.on('example', (event, arg) => {
-        this.setState({
-          arg: arg
-        });
-      })
-  }
-
-  handleClick() {
-      ipcRenderer.send('example', 'Hello World');
-  }
-
-  render() {
-    const { arg } = this.state;
-
-    return (
-      <div>
-        <h1>{ arg }</h1>
-        <button onClick={ this.handleClick }>Click</button>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<span>Hi</span>
+			</div>
+		);
+	}
 }
 
 export default App;
